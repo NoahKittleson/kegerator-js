98a635842305d8c54c63d6a34b85e9f337ebaec8
Kendra Dunham and Noah Kittleson <Guest@epicodus-55.local>
2016-06-02 09:09:25 -0700

    testing

--- app/keg-display.component.ts
+++ app/keg-display.component.ts
@@ -6,8 +6,10 @@ import { Keg } from './keg.model';
   inputs: ['keg'],
   template: `
     <div class="container">
+    <span *inIf="keg.pints === 91"
       <label (click) = "toggleShow()">{{ keg.name }} {{ keg.type }} {{ keg.ABV }}</label>
-      <p *ngIf="show === true">{{ keg.pints }} {{ keg.empty }}</p>
+      <p *ngIf="show === true">Pints Left in Keg: {{ keg.pints }} Keg Empty? {{ keg.empty }}</p>
+      <button (click)="takePint()" id="overwrite">TAKE PINT</button>
     </div>
   `
 })
@@ -16,5 +18,19 @@ export class KegDisplayComponent {
   public show: boolean = false;
   toggleShow() {
     this.show = !this.show;
+  }
+  takePint() {
+    this.keg.pints -= 1;
+  }
+  kegLeft() {
+    if(this.keg.pints > 93) {
+      return 4;
+    } else if(this.keg.pints > 62) {
+      return 3;
+    }else if(this.keg.pints > 31) {
+      return 2;
+    } else {
+      return 1;
+    }
   }
 }
